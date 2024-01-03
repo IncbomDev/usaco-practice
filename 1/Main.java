130c991fe1460d7402d7ca09a488c48c9dc1addd
Incbom <99501939+IncbomDev@users.noreply.github.com>
2024-01-03 13:42:40 -0500

    leetcode

--- 1/Main.java
+++ 1/Main.java
@@ -1,20 +1,32 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String obj = scanner.nextLine();
-        String[] strings = obj.split(" ");
-        int[] nums = new int[strings.length];
-        for (int i = 0; i < strings.length; i++) {
-            nums[i] = Integer.parseInt(strings[i]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader("./ride.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
+
+        String groupA = f.readLine();
+        String groupB = f.readLine();
+
+        if (groupA != null && groupB != null) {
+            System.out.println(groupA);
+            System.out.println(groupB);
+            out.println(groupB);
+        } else {
+            System.out.println("Not enough lines in the file");
         }
-        Arrays.sort(nums);
-        int a = nums[0];
-        int b = nums[1];
-        int c = nums[4];
-        System.out.println(a + " " + b + " " + c);
+
+        f.close();
+        out.close();
+
+    }
+
+    public static int getCharNumber(char c) {
+        char[] chars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+        for (char s : chars) {
+            return c - 'A' + 1;
+        }
+        return 0;
     }
 }
