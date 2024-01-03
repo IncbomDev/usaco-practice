c17b46b7984da59efe0b7067833b5eb16e917800
Incbom <99501939+IncbomDev@users.noreply.github.com>
2024-01-03 14:42:49 -0500

    rip compiler

--- 1/Ride.java
+++ 1/Ride.java
@@ -2,21 +2,19 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+
+/*
+ID: nathane3
+LANG: JAVA
+PROG: ride
+*/
+class Ride {
     public static void main(String[] args) throws IOException {
         BufferedReader f = new BufferedReader(new FileReader("./ride.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
 
         String groupA = f.readLine();
         String groupB = f.readLine();
-
-        if (groupA != null && groupB != null) {
-            System.out.println(groupA);
-            System.out.println(groupB);
-            out.println(groupB);
-        } else {
-            System.out.println("Not enough lines in the file");
-        }
 
         assert groupB != null;
         List<Integer> groupBNum = new ArrayList<>();
@@ -28,6 +26,27 @@ public class Main {
         for (char c : groupA.toCharArray()) {
             groupANum.add(getCharNumber(c));
         }
+        int resultA = 1;
+        for (int i : groupANum) {
+            resultA *= i;
+        }
+
+        int resultB = 1;
+        for (int i : groupBNum) {
+            resultB *= i;
+        }
+
+        if (resultA % 47 == 27) {
+            out.println("GO");
+        } else {
+            out.println("STAY");
+        }
+
+        if (resultB % 47 == 27) {
+            out.println("GO");
+        } else {
+            out.println("STAY");
+        }
 
         f.close();
         out.close();
@@ -37,9 +56,12 @@ public class Main {
     public static int getCharNumber(char c) {
         char[] chars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-        for (char s : chars) {
-            return c - 'A' + 1;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == c) {
+                return i + 1;
+            }
         }
         return 0;
     }
+
 }
